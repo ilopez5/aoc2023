bd42619d4f190cf5cd1cac9a48bb897c73c660e7
Ismael J Lopez <ismaelopezandrade@gmail.com>
2023-11-30 19:57:48 -0600

    prep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("inputs/placeholder.txt");
+    let lines: Vec<&str> = input.lines().collect();
+    
+    let part1 = part1();
+    println!("Part 1: {}", part1);
+    
+    let part2 = part2();
+    println!("Part 2: {}", part2);
+}
+
+fn part1() -> usize {
+    0
+}
+
+
+fn part2() -> usize {
+    0
 }
